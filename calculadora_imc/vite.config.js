526b224c012e8f59559827eacaad7293919bc6ea
leandrodiascarvalho <leandrovinte100@gmail.com>
2025-08-16 20:34:53 -0300

    Adiciona workflow deploy para Github Pages

--- calculadora_imc/vite.config.js
+++ calculadora_imc/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/leandrodiascarvalho/imc_react/tree/main/calculadora_imc",
+  base: "/calculadora_imc",
 });
